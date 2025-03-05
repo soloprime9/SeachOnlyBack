2d32ba611eeb551d38095f4d64780b95894a197e
soloprime9 <primeindia5750@gmail.com>
2025-03-05 19:35:20 +0530

    hello

--- demo.py
+++ demo.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app, origins=["http://localhost:3000"])
+CORS(app, origins=["https://search-beta-six.vercel.app/"])
 
 @app.route("/search", methods=['GET'])
 
